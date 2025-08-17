37b6df46cd8378873c6cbca177a88f45a39d45ee
Vasanth <vasanthravi66@gmail.com>
2025-08-17 08:55:41 +0530

    updata

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/LA-cinema-ticket-booking-web/",
   plugins: [
     react(),
      tailwindcss(),
